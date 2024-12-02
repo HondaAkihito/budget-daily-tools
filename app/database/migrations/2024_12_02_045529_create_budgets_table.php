10868b9343a170cb8b2ffee57a5260c8338dbe8c
HondaAkihito <honaki.and.1114@gmail.com>
2024-12-02 21:33:39 +0900

    “20241202-Migration”

---
+++ app/database/migrations/2024_12_02_045529_create_budgets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBudgetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('budgets', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', '20');
+            $table->integer('amount');
+            $table->date('from_date');
+            $table->date('to_date');
+            $table->integer('rest_amount')->nullable();
+            $table->integer('day_amount')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('budgets');
+    }
+}
